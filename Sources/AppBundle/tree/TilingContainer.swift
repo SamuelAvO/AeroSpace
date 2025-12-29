51e3c0f1b491ef4f0e2a72b8d1c5a53c8d723e2c
Samuel van Oostveen <samuelvanoostveen@gmail.com>
2025-12-29 23:21:44 +0000

    more layout reverts

--- Sources/AppBundle/tree/TilingContainer.swift
+++ Sources/AppBundle/tree/TilingContainer.swift
@@ -7,26 +7,19 @@ final class TilingContainer: TreeNode, NonLeafTreeNodeObject { // todo consider 
     var layout: Layout
 
     @MainActor
-    init(
-        parent: NonLeafTreeNodeObject, adaptiveWeight: CGFloat, _ orientation: Orientation,
-        _ layout: Layout, index: Int
-    ) {
+    init(parent: NonLeafTreeNodeObject, adaptiveWeight: CGFloat, _ orientation: Orientation, _ layout: Layout, index: Int) {
         self._orientation = orientation
         self.layout = layout
         super.init(parent: parent, adaptiveWeight: adaptiveWeight, index: index)
     }
 
     @MainActor
-    static func newHTiles(parent: NonLeafTreeNodeObject, adaptiveWeight: CGFloat, index: Int)
-        -> TilingContainer
-    {
+    static func newHTiles(parent: NonLeafTreeNodeObject, adaptiveWeight: CGFloat, index: Int) -> TilingContainer {
         TilingContainer(parent: parent, adaptiveWeight: adaptiveWeight, .h, .tiles, index: index)
     }
 
     @MainActor
-    static func newVTiles(parent: NonLeafTreeNodeObject, adaptiveWeight: CGFloat, index: Int)
-        -> TilingContainer
-    {
+    static func newVTiles(parent: NonLeafTreeNodeObject, adaptiveWeight: CGFloat, index: Int) -> TilingContainer {
         TilingContainer(parent: parent, adaptiveWeight: adaptiveWeight, .v, .tiles, index: index)
     }
 }
